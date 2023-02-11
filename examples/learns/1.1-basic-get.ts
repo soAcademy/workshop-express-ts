108fa104e81e2777bc5aeb9210972d203457e477
Panachai <panachai.ny@gmail.com>
2023-02-11 10:29:59 +0700

    add

--- examples/learns/1.1-basic-get.ts
+++ examples/learns/1.1-basic-get.ts
@@ -1,7 +1,9 @@
 import express from "express";
 const app = express();
 
-app.get("/menus", (_, res) => {
+type ReqQuery = { limit?: number };
+
+app.get("/menus", (req: express.Request<{}, {}, {}, ReqQuery>, res) => {
   const mockResponseBody = [
     {
       id: 1,
@@ -26,7 +28,30 @@ app.get("/menus", (_, res) => {
     },
   ];
 
+  const limit = req.query.limit;
+
+  if (limit) {
+    return res.send(
+      mockResponseBody.filter((element, index, array) => index < limit)
+    );
+  }
+
   return res.send(mockResponseBody);
+});
+
+type ReqParams = { id: number };
+
+app.get("/menus/:id", (req: express.Request<ReqParams, {}, {}, {}>, res) => {
+  // TODO: will use for query in database
+  const { id } = req.params;
+
+  return res.send({
+    id: 1,
+    name: "ผัดกระเพราไก่",
+    price: 50,
+    current_price: 40,
+    category_name: "ผัด",
+  });
 });
 
 app.listen(5000);
