413ea194159141475483a8a15d6f6e0eeede7897
Panachai <panachai.ny@gmail.com>
2023-02-11 10:05:18 +0700

    Update 1-basic-get.ts

--- examples/learns/1-basic-get.ts
+++ examples/learns/1-basic-get.ts
@@ -40,4 +40,19 @@ app.get("/menus", (req: express.Request<{}, {}, {}, ReqQuery>, res) => {
   return res.send(mockResponseBody);
 });
 
+type ReqParams = { id: number };
+
+app.get("/menus/:id", (req: express.Request<ReqParams, {}, {}, {}>, res) => {
+  // TODO: will use for query in database
+  const { id } = req.params;
+
+  return res.send({
+    id: 1,
+    name: "ผัดกระเพราไก่",
+    price: 50,
+    current_price: 40,
+    category_name: "ผัด",
+  });
+});
+
 app.listen(5000);
