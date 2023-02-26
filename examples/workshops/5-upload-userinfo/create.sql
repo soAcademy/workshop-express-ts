1a1942026fb019d4582ac24035349c4f8fb25c37
Panachai <panachai.ny@gmail.com>
2023-02-26 15:52:23 +0700

    Create create.sql

---
+++ examples/workshops/5-upload-userinfo/create.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE upload_userinfo;
+
+-- public.files definition
+-- Drop table
+-- DROP TABLE public.files;
+CREATE TABLE public.files (
+    id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
+    filename varchar NOT NULL,
+    mimetype varchar NOT NULL,
+    destination varchar NOT NULL,
+    CONSTRAINT files_pk PRIMARY KEY (id)
+);
+
+-- public.userinfos definition
+-- Drop table
+-- DROP TABLE public.userinfos;
+CREATE TABLE public.userinfos (
+    id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
+    first_name varchar NULL,
+    last_name varchar NULL,
+    CONSTRAINT userinfos_pk PRIMARY KEY (id),
+    CONSTRAINT userinfos_fk FOREIGN KEY (id) REFERENCES public.files(id)
+);
