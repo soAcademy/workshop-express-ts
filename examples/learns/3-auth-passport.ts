176b03f79a0eca9e4b75918f3b06ac89cb47b68f
Panachai <panachai.ny@gmail.com>
2023-02-28 00:15:18 +0700

    Create 3-auth-passport.ts

---
+++ examples/learns/3-auth-passport.ts
@@ -0,0 +1,37 @@
+import express, { Request, Response } from "express";
+import bcrypt from "bcrypt-nodejs";
+import * as jwt from "jsonwebtoken";
+import passport from "passport";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+
+const jwtSecret = "szxczxcasdasd";
+
+type UserModel = {
+  username: String;
+  password: String;
+};
+
+app.post("/login", (req: Request<{}, {}, UserModel, {}>, res: Response) => {
+  const hashedPassword = bcrypt.hashSync(
+    req.body.password,
+    bcrypt.genSaltSync(10)
+  );
+
+  // TODO: connect to db and save user
+  // await User.create({
+  //   username: req.body.username,
+  //   password: hashedPassword,
+  // });
+
+  const token = jwt.sign(
+    { username: req.body.username }, //, scope: req.body.scope
+    jwtSecret
+  );
+
+  res.status(200).send({ token: token });
+});
+
+app.listen(5000, () => console.log("Server started on port 5000"));
