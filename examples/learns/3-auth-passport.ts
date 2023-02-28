a84594583342098a452c8a9eff356b51de51474a
Panachai <panachai.ny@gmail.com>
2023-02-28 23:04:09 +0700

    Update 3-auth-passport.ts

--- examples/learns/3-auth-passport.ts
+++ examples/learns/3-auth-passport.ts
@@ -1,13 +1,15 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import bcrypt from "bcrypt-nodejs";
 import * as jwt from "jsonwebtoken";
 import passport from "passport";
 import bodyParser from "body-parser";
+import passportJwt from "passport-jwt";
+const ExtractJwt = passportJwt.ExtractJwt;
 
 const app = express();
 app.use(bodyParser.json());
 
-const jwtSecret = "thisisexamplejwtsecret";
+const JWTSecret = "thisisexamplejwtsecret";
 
 type UserModel = {
   username: String;
@@ -28,7 +30,7 @@ app.post("/register", (req: Request<{}, {}, UserModel, {}>, res: Response) => {
 
   const token = jwt.sign(
     { username: req.body.username }, //, scope: req.body.scope
-    jwtSecret
+    JWTSecret
   );
 
   res.status(201).send({ token: token });
@@ -48,10 +50,57 @@ app.post("/login", (req: Request<{}, {}, UserModel, {}>, res: Response) => {
 
   const token = jwt.sign(
     { username: req.body.username }, //, scope: req.body.scope
-    jwtSecret
+    JWTSecret
   );
 
   res.status(200).send({ token: token });
 });
 
+const JwtStrategy = passportJwt.Strategy;
+passport.use(
+  new JwtStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: JWTSecret,
+    },
+    function (jwtToken, done) {
+      // TODO: implement compare username in db
+      // NOTE: error case
+      // if (err) {
+      //   return done(err, false);
+      // }
+
+      // NOTE: success case
+      // if (user) {
+      const mockUser = {
+        username: jwtToken.username,
+      };
+      return done(undefined, mockUser, jwtToken);
+      // } else {
+      // NOTE: not have user case
+      // return done(undefined, false);
+      // }
+    }
+  )
+);
+
+const authenticateJWT = (req: Request, res: Response, next: NextFunction) => {
+  passport.authenticate("jwt", function (err, user, info) {
+    if (err) {
+      console.log(err);
+      return res.status(401).json({ status: "error", code: "unauthorized" });
+    }
+    if (!user) {
+      return res.status(401).json({ status: "error", code: "unauthorized" });
+    } else {
+      return next();
+    }
+  })(req, res, next);
+};
+
+app.get("/example-protect", authenticateJWT, (req: Request, res: Response) => {
+  res.status(200).send({ message: "access passed" });
+});
+
 app.listen(5000, () => console.log("Server started on port 5000"));
+
